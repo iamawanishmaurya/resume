fe8b7df053598180eeaef974a3ef329254c535df
iamawanishmaurya <awanishmaurya9192@gmail.com>
2026-01-01 09:14:31 +0530

    Update assets/js/main.js

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
