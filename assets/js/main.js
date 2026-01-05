c71b20ffe402d40ee2325cc0a3be70cdf2772158
iamawanishmaurya <awanishmaurya9192@gmail.com>
2026-01-05 16:34:54 +0530

    Update assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,9 @@ const iconTheme = 'bx-sun'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
