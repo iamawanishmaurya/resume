645ea3ec62b688e730014d5109dbfaa52497320e
iamawanishmaurya <awanishmaurya9192@gmail.com>
2026-01-08 20:46:45 +0530

    Update assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,3 +118,9 @@ function generateResume(){
 // When the button is clicked, it executes the three functions
 resumeButton.addEventListener('click', () =>{
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+    scaleCv()
+
+    // 2. The PDF is generated
+    generateResume()
+
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
